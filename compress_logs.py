c2b89b9849207f45fc85670c3da3b7e098b50362
Sanghyun Kim <sanghyun.kim@vivity.ai>
2026-08-13 23:04:24 +0900

    compress_logs.py

---
+++ compress_logs.py
@@ -0,0 +1,45 @@
+import collections
+
+def compress_logs(logs):
+
+    que = collections.deque(logs[1:])
+
+    curr_txt = logs[0][0]
+    curr_seconds = logs[0][1]
+    start_seconds = logs[0][1]
+    curr_compress_log = (curr_txt, start_seconds, curr_seconds)
+
+    compressed_logs = []
+    while que:
+        log = que.popleft()
+        txt = log[0]
+        seconds = log[1]
+        if (curr_txt == txt) & (curr_seconds == seconds-1):
+            curr_compress_log = (curr_txt, start_seconds, seconds)
+            curr_seconds = seconds
+
+        else:
+            compressed_logs.append(curr_compress_log)
+            curr_txt = txt
+            curr_seconds = seconds
+            start_seconds = seconds
+            curr_compress_log = (curr_txt, start_seconds, curr_seconds)
+
+    compressed_logs.append(curr_compress_log)
+    return compressed_logs
+
+
+if __name__ == '__main__':
+
+    logs = [
+    ("login", 1),
+    ("login", 2),
+    ("login", 3),
+    ("logout", 10),
+    ("error", 15),
+    ("error", 16),
+    ("error", 17),
+    ("login", 30)
+    ]
+
+    print(compress_logs(logs))

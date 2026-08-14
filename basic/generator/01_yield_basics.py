55063b9313bb9c28dc7ce8aa9a2a87051322ace8
Sanghyun kim <sanghyun9082@gmail.com>
2026-08-14 13:41:35 +0900

    generator

---
+++ basic/generator/01_yield_basics.py
@@ -0,0 +1,23 @@
+def count_up_to(to:int):
+    print("start")
+    num = 0
+    while num < to:
+        num += 1
+        yield num
+
+
+if __name__ == "__main__":
+
+    g = count_up_to(3)
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    # print(next(g))
+
+    print("-------------")
+    print(hasattr(g, "__iter__"))
+    print(hasattr(g, "__next__"))
+
+    # for문은 내부적으로 next()를 반복하다가, StopIteration을 잡아서 루프 종료 신호로 쓴다.
+    for n in count_up_to(3):
+        print(n)
